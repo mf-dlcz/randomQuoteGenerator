2b33c74645820df8f96bdedd77cca04fc9857b0e
mariadlcruzz0-0 <mariadlcruzz0.0@gmail.com>
2022-09-12 15:19:03 -0700

    Created an array and added 5 objects. Each object has a key and a property value.

--- js/script.js
+++ js/script.js
@@ -8,16 +8,47 @@ project 1 - A Random Quote Generator
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
- * `quotes` array 
+ * `quotes` array with 5 objects: keys and their property values
 ***/
-
-
+let quotes = [ 
+  {
+    quote: 'Either you run the day or the day runs you',
+    source: 'Jim Rohn'
+  },
+  {
+    quote: 'Just one small positive thought in the morning can change your whole day',
+    source: 'Dalai Lama',
+    year: 1850
+  },
+  {
+    quote: 'Opportunities don\'t happen, you create them.',
+    source: 'Chris Grosser'
+  },
+  {
+    quote: 'Life is like riding a bicycle. To keep your balance you must keep moving.',
+    source: 'Albert Einstein'
+  },
+  {
+    quote: 'You don\'t always need a plan. Sometimes you just need to breathe, trust, let go and see what happens.',
+    source: 'Mandy Hale',
+    citation: 'Twitter'
+  }
+];
 
 /***
  * `getRandomQuote` function
 ***/
 
+function getRandomQuote() {
+  // 1. Create a variable that generates a random number
+  // between zero and the last index in the `quotes` array
 
+  // 2. Use the random number variable and bracket notation 
+  // to grab a random object from the `quotes` array, and 
+  // store it in a variable
+
+  // 3. Return the variable storing the random quote object
+}
 
 /***
  * `printQuote` function
