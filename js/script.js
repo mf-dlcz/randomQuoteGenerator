0c223db2db1d15202abe87fe27458260e127c382
mariadlcruzz0-0 <mariadlcruzz0.0@gmail.com>
2022-09-13 15:30:16 -0700

    Removed the instructions of my code and changed my comments to explain what each section of my code does. I also changed the variable declarations to const to avoid any alterations.

--- js/script.js
+++ js/script.js
@@ -8,9 +8,10 @@ project 1 - A Random Quote Generator
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
- * `quotes` array has 5 objects: Their keys and their property values (quotes, source, year, tag, and citation)
+ * `quotes` 
+ * array has 5 objects: Their keys and their property values (quotes, source, year, tag, and citation)
 ***/
-let quotes = [ 
+const quotes = [ 
   {
     quote: 'Either you run the day or the day runs you',
     source: 'Jim Rohn',
@@ -51,70 +52,43 @@ let quotes = [
 
 /***
  * `getRandomQuote` function
+ * generates a random quote that is stored in randomQuote
 ***/
 
 function getRandomQuote() {
-  // 1. Create a variable that generates a random number
-  // between zero and the last index in the `quotes` array
 
-  let randomNumb = Math.floor(Math.random()* (quotes.length));
+  const randomNumb = Math.floor(Math.random()* (quotes.length));
+  const randomQuote = quotes[randomNumb];
 
-  // 2. Use the random number variable and bracket notation 
-  // to grab a random object from the `quotes` array, and 
-  // store it in a variable
-
-  let randomQuote = quotes[randomNumb];
-
-  // 3. Return the variable storing the random quote object
   //console.log(randomQuote);
   //console.log(randomNumb);
   return randomQuote;
 }
 
 /***
- * `printQuote` function
+ * `printQuote` 
+ * printQuote calls getRandomQuote(), uses the returned randomQuote object and 
+ * uses a string to display a random quote.  
 ***/
-/**printQuote calls getRandomQuote(). stringQuote holds the strings inside each property value
- * (quote, source, year, tag, citation).**/
+
 function printQuote() {
 
-   // 1. Create a variable that calls the getRandomQuote() 
-  // function
-
   let displayRandomQuote = getRandomQuote();
-
-  // 2. Create a variable that initiates your HTML string with 
-  // the first two <p></p> elements, their classNames, 
-  // and the quote and source properties, but leave off 
-  // the second closing `</p>` tag for now
-
   let stringQuote = `<p class='quote'> ${displayRandomQuote.quote} </p>
-  <p class='source'> ${displayRandomQuote.source} `
-
-  // 3. Use an if statement to check if the citation property 
-  // exists, and if it does, concatenate a <span></span> 
-  // element, appropriate className, and citation property 
-  // to the HTML string
+    <p class='source'> ${displayRandomQuote.source} `
 
   if( displayRandomQuote.citation !== undefined ) {
     stringQuote +=`<span class='citation'>${displayRandomQuote.citation}</span>`
   }
 
-  // 4. Use an if statement to check of the year property exists, 
-  // and if it does, concatenate a <span></span> element, 
-  // appropriate className, and year property to the HTML 
-  //string
   if( displayRandomQuote.year !== undefined ) {
     stringQuote += `<span class='year'>${displayRandomQuote.year}</span>`
 }
   if( displayRandomQuote.tag !== undefined ) {
     stringQuote += `<span class='tag'>${displayRandomQuote.tag}</span>`
   }
-  // 5. After the two if statements, concatenate the closing </p> 
-  // tag to the HTML string
-  '</p>'
-  // 6. set the innerHTML of the quote-box div to equal the 
-  // complete HTML string
+    '</p>'
+  
   return (document.getElementById('quote-box').innerHTML = stringQuote);
 }
   
